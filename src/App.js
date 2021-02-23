91070404185fca7689cf3a0c3e27ab66640702e2
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2021-02-23 23:48:06 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <View>
+       <text> 
+          React-native
+       </text>
+     </View>
+    );
+  }
+}
