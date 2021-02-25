708e7f61aa4d8c7e41b61711f83b825cc4d39ec5
MichelWakim <50673472+MichelWakim@users.noreply.github.com>
2021-02-25 09:09:10 +0200

    Creating Data Creating Dummy Data in the redux store

---
+++ src/utils/_DATA.js
@@ -0,0 +1,70 @@
+export const decks = {
+  React: {
+    title: "React",
+    questions: [
+      {
+        question: "What is React?",
+        answer: "A library for managing user interfaces"
+      },
+      {
+        question: "What is JSX?",
+        answer:
+          "Stands for JavaScript XML. It allows combining JavaScript with HTML. Results in easier implementation and clean code."
+      },
+      {
+        question: "Where do you make Ajax requests in React?",
+        answer: "The componentDidMount lifecycle event"
+      }
+    ]
+  },
+  JavaScript: {
+    title: "JavaScript",
+    questions: [
+      {
+        question: "What is a closure?",
+        answer:
+          "The combination of a function and the lexical environment within which that function was declared."
+      }
+    ]
+  },
+  Redux: {
+    title: "Redux",
+    questions: [
+      {
+        question: "What is Redux?",
+        answer: "A predictable state container for JavaScript Apps"
+      },
+      {
+        question: "What is an action creator?",
+        answer:
+          "It is a function that takes an input and returns an object with a type and data property."
+      },
+      {
+        question: "What is a reducer?",
+        answer:
+          "A reducer is a pure function that takes the current state and action and returns the next state."
+      }
+    ]
+  },
+  "HTML and CSS": {
+    title: "HTML and CSS",
+    questions: [
+      {
+        question: "What does HTML stand for?",
+        answer: "Hyper Text Markup Language"
+      },
+      {
+        question: "Who is making the Web standards?",
+        answer: "The World Wide Web Consortium"
+      },
+      {
+        question: "What's the tag to create a numbered list?",
+        answer: "<ol>"
+      },
+      {
+        question: "What does CSS stand for?",
+        answer: "Cascading Style Sheets"
+      }
+    ]
+  }
+};
